a89de451396693c79419e85490f69c5020951972
zenith <reeingspam@gmail.com>
2022-03-29 20:50:13 -0700

    react test

---
+++ component.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div className="home flex-col-hstart-vstart clip-contents">
+      <div className="rectangle-1" />
+      <div className="group-341 flex-col-hcenter">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/wjze01fael-4%3A10?alt=media&token=c3ee05f2-5161-4ce5-9294-7077b7f940df"
+          alt="Not Found"
+          className="banner"
+        />
+        <p className="txt-359 flex-hcenter">JOIN US: IP</p>
+        <div className="rectangle-3" />
+      </div>
+    </div>
+  );
+}
