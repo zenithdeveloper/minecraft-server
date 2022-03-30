dd9397f5d40425c6ef559e2c01f3e22e406458a2
zenith <reeingspam@gmail.com>
2022-03-29 21:12:58 -0700

    react rollback v2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./styles.css"
+
+export default function Home() {
+  return (
+    <div className="home flex-col-hstart-vstart clip-contents">
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/hzglfkaxmeb-4%3A9?alt=media&token=ba63af41-08e6-4361-9ad7-7f84a9104949"
+        alt="Not Found"
+        className="image-1"
+      />
+      <div className="rectangle-3" />
+      <div className="rectangle-1" />
+      <p className="txt-432 flex-hcenter">JOIN US: IP</p>
+    </div>
+  )
+}
